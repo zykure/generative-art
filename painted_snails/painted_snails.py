c2abbc79bc9cd01fb84667fe435c6254c2474ef5
Jan Behrens <jan.behrens@kit.edu>
2021-06-05 10:11:44 +0200

    Update painted snails

--- painted_snails/painted_snails.py
+++ painted_snails/painted_snails.py
@@ -6,16 +6,45 @@ from threading import Thread
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import argparse
 
-# TODO: determine distance from origin (n_rotations)
+parser = argparse.ArgumentParser(description='Produce some generative artwork.')
+parser.add_argument('--seed', type=int, default=None,
+                    help='Seed for the algorithm (default: random)')
+parser.add_argument('--size', type=int, default=400,
+                    help='Size of a single output image')
+parser.add_argument('--mult', metavar='N', type=int, default=1,
+                    help='Merge N*N images into single output file')
+parser.add_argument('--oversample', metavar='S', type=float, default=1,
+                    help='Apply S-fold oversampling (render images with S times resolution)')
+parser.add_argument('--ratio', type=float, default=0.137,
+                    help='Opening ratio of the logarithmic spiral')
+parser.add_argument('--output_path', type=str, default='output',
+                    help='Output path (default: "./output")')
+parser.add_argument('--debug', action='store_true',
+                    help='Enable debug mode')
 
-def random1d(rng, n):
+args = parser.parse_args()
+
+N_ROWS_COLS = args.mult
+IMG_SIZE = args.size
+OUTPUT_SIZE = IMG_SIZE * N_ROWS_COLS
+
+DEBUG = args.debug
+SEED = args.seed if args.seed != None else np.random.randint(65536)
+
+OVERSAMPLE = args.oversample
+SIZE = int(OUTPUT_SIZE/2 * OVERSAMPLE) // N_ROWS_COLS
+RATIO = args.ratio
+
+
+def random1d(rng, n, k=2):
     # create a 2D array of random numbers
     x = np.linspace(0, 1, n)
     z = rng.random(n)
 
     # return an interpolation function
-    f = interpolate.UnivariateSpline(x, z, k=3)
+    f = interpolate.UnivariateSpline(x, z, k=k)
 
     if DEBUG:
         xr = np.linspace(0, 1, 100)
@@ -25,25 +54,23 @@ def random1d(rng, n):
         plt.tight_layout()
         plt.show()
 
-
     return f
 
-def random2d(rng, n, m):
+def random2d(rng, n, m, k=2):
     # create a 2D array of random numbers
     x = np.linspace(0, 1, n)
-    y = np.linspace(0, 2*np.pi, m+1)
-    #z = rng.random((m,n))
-    z = rng.normal(loc=rng.random(), scale=0.5*rng.random(), size=(m,n))
+    y = np.linspace(0, 1, m+1)
+    z = rng.random((m,n))
 
     # avoid invalid color values
     # based on lmfit, see: https://lmfit.github.io/lmfit-py/bounds.html
-    z = (np.sin(z) + 1) * 0.5
+    #z = (np.sin(z) + 1) * 0.5
 
     # make it periodic in y
     z = np.vstack([z, z[0,:]])
 
     # return an interpolation function
-    f = interpolate.RectBivariateSpline(x, y, z.T, kx=3, ky=3)
+    f = interpolate.RectBivariateSpline(x, y, z.T, kx=k, ky=k)
 
     if DEBUG:
         plt.title('n=%d m=%d seed=%d' % (n,m,seed))
@@ -51,7 +78,7 @@ def random2d(rng, n, m):
         #plt.scatter(xx, yy, c='k', marker='o')
         #plt.scatter(xx, yy, c=z, marker='.', cmap='gist_rainbow')
         xr = np.linspace(0, 1, 100)
-        yr = np.linspace(0, 2*np.pi, 100)
+        yr = np.linspace(0, 1, 100)
         zr = f(xr, yr)
         zr[zr < 0] = np.nan
         zr[zr > 1] = np.nan
@@ -74,11 +101,16 @@ def spiral(x, y, a=1.0, b=1.0, r_max=100, t_start=0):
 
     # early exit if the point requested is the origin itself
     # to avoid taking the logarithm of zero in the next step
-    if r == 0:
+    if r <= 1:
         return (0,0,0)
 
     if r > r_max:
         return (0,0,1)
+
+    # calculate angle of current position
+    k = np.log(r/a)/b
+    k_max = np.log(r_max/a)/b
+    k_scale = k / k_max
 
     # calculate the floating point approximation for n
     n = (np.log(r/a)/b - t) / (2*np.pi)
@@ -95,31 +127,20 @@ def spiral(x, y, a=1.0, b=1.0, r_max=100, t_start=0):
     r_dist = abs(r - lower_r)
     r_scale = r_dist / width
 
-    t_scale = t
+    t_scale = t / (2*np.pi)
 
-    if r_scale < 0.02 + 0.01 * random_R(t / (2*np.pi)):
+    if r_scale < 0.02 + 0.01 * random_R(k_scale):
         return (0,0,0)
 
     # return the minimum distance to the target point
     return (
-        random_H(r_scale, t_scale),
-        0.2 + 0.6 * random_S(r_scale, 0) + 0.2 * random_S(0, t_scale),
-        0.5 + 0.5 * random_V(np.sqrt(r_scale), 0),
+        random_H(r_scale, k_scale),
+        0.2 + 0.6 * random_S(r_scale, 0) + 0.1 * random_S(0, t_scale) + 0.1 * random_S(0, k_scale),
+        np.exp(random_V(np.sqrt(r_scale), 0)),
     )
 
 # produce a PNM image of the result
 if __name__ == '__main__':
-
-    N_ROWS_COLS = 2
-    IMG_SIZE = 400
-    OUTPUT_SIZE = IMG_SIZE * N_ROWS_COLS
-
-    DEBUG = False
-    SEED = np.random.randint(65536)
-
-    OVERSAMPLE = 2
-    SIZE = int(OUTPUT_SIZE/2 * OVERSAMPLE) // N_ROWS_COLS
-    RATIO = 0.137
 
     out_im = Image.new('RGB', (OUTPUT_SIZE,OUTPUT_SIZE))
 
@@ -134,10 +155,10 @@ if __name__ == '__main__':
         # initialize random generator
         rng = np.random.default_rng(seed)
 
-        random_R = random1d(rng, 11)
-        random_H = random2d(rng, 23, 3)
-        random_S = random2d(rng, 23, 137)
-        random_V = random2d(rng, 11, 5)
+        random_R = random1d(rng, 23)
+        random_H = random2d(rng, 7, 2)
+        random_S = random2d(rng, 7, 137)
+        random_V = random2d(rng, 13, 23)
 
         r_max = 0.9 * SIZE
         t_start = rng.random() * 2*np.pi
